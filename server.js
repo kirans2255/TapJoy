6925c947d90626a6558a5a6690a25d90efe4faee
kirans2255 <kiransunilkumar2017@gmail.com>
2024-08-13 10:22:16 +0530

    updates

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const cookieParser = require('cookie-parser');
 
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 // Connect to MongoDB
 mongoose.connect(process.env.MONGODB_CONNECT);
